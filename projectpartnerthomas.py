3f5862af37ae1a9b868db4fbb20cf4d4948a4e37
kingyints-cpu <kingyints@gmail.com>
2026-01-22 14:15:48 -0600

    Remove movie genres and titles from projectpartnerthomas.py
    Removed a list of movie genres and titles.

--- projectpartnerthomas.py
+++ projectpartnerthomas.py
@@ -1,81 +1,102 @@
-Action
+hi
 
-Mad Max: Fury Road
 
-John Wick
 
-Die Hard
 
-Comedy
 
-Superbad
 
-Mean Girls
 
-The Hangover
 
-Drama
 
-Forrest Gump
 
-The Shawshank Redemption
 
-Fight Club
 
-Horror
 
-Get Out
 
-A Quiet Place
 
-The Conjuring
 
-Romance
 
-The Notebook
 
-Titanic
 
-La La Land
 
-Science Fiction (Sci-Fi)
 
-Inception
 
-Interstellar
 
-The Matrix
 
-Fantasy
 
-Harry Potter and the Sorcerer’s Stone
 
-The Lord of the Rings: The Fellowship of the Ring
 
-Pan’s Labyrinth
 
-Thriller
 
-Gone Girl
 
-Se7en
 
-Black Swan
 
-Animation
 
-Toy Story
 
-Spider-Man: Into the Spider-Verse
 
-Finding Nemo
 
-Documentary
 
-March of the Penguins
 
-13th
 
-Free Solo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
